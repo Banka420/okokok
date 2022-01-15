a5cb1421bbfe64e3eafbcb0becf9215dca6eb7df
Aditya <adityabanka08@gmail.com>
2022-01-15 20:38:50 +0530

    commit

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
 import pygame
+import random
 
+#initialising pygame
 pygame.init()
 
+#making the screen
 screen = pygame.display.set_mode((800,600))
 
+#for setting font
+base_font = pygame.font.Font(None , 32)
 
+#setting title and logo
 pygame.display.set_caption("OP")
 icon = pygame.image.load('football.png')
 pygame.display.set_icon(icon)
 
+#starting position of ball (basically centre)
 iconX = 400
 iconY= 300
 
+
 def football(x,y):
     screen.blit(icon , (x,y))
+
+
+#making the question
+n1 = random.randint(0,100)
+n2 = random.randint(0,100)
+ans1 = n1 + n2
+
+nn1 = str(n1)
+nn2 = str(n2)
+nn3 = nn1 + " + " + nn2
+question = ("What is " + nn3 + "?")
+
 
 
 running = True
@@ -22,10 +42,16 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    iconX += 0.05
+    iconX += 0.02
 
-
+#for making background green
     screen.fill ((0,255,0))
 
+#for making the football appear
     football(iconX,iconY)
+
+#for printing the question
+    questionbox = base_font.render( question, True, (0,0,0))
+    screen.blit(questionbox, (0,0))
+
     pygame.display.update()
